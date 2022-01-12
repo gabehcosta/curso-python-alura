9c3b8fbd55b879e37e4bda38089d37d45de07b66
Gabriel Costa <g.h.costa@outlook.com>
2022-01-12 10:33:37 -0300

    Criando a lógica do jogo de forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,14 @@ def jogar():
     print("*  Seja Bem vindo ao jogo de Forca!  *")
     print("**************************************")
 
+    palavra_secreta = "banana"
+
+    acertou = False
+    enforcou = False
+
+    while(not acertou and not enforcou):
+        print("Continue jogando...")
+
     print("\nFim de Jogo!")
 
 if(__name__ == "__main__"):
