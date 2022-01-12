6ef7cd3c113517027a8c22e083bfa5ac8f2fbaaa
Gabriel Costa <g.h.costa@outlook.com>
2022-01-12 14:43:46 -0300

    Finalizando logica do jogo de forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,22 +4,45 @@ def jogar():
     print("*  Seja Bem vindo ao jogo de Forca!  *")
     print("**************************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_certas = ["_" for letra in palavra_secreta]
+
+    print(letras_certas)
 
     acertou = False
     enforcou = False
+    
+    tentativas = 5
+    print("Você tem {} tentativas".format(tentativas))
 
     while(not acertou and not enforcou):
 
         chute = input("Digite uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Letra {} foi encontrada na posição {}".format(letra, index))
-            
-            index += 1
+        if(chute in palavra_secreta):
+
+            index = 0
+            for letra in palavra_secreta:
+                
+                if(chute == letra):
+                    letras_certas[index] = letra
+
+                index += 1
+
+        else:
+            tentativas -= 1
+            print("Você tem {} tentativas".format(tentativas))
+
+        enforcou = tentativas == 0
+        acertou = "_" not in letras_certas
+
+        print(letras_certas)
+
+        if(acertou):
+            print("Parabéns, você ganhou!!!")
+        elif(enforcou):
+            print("Que pena, você perdeu!!")
 
     print("\nFim de Jogo!")
 
