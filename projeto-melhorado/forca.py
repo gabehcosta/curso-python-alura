dd0114ad9244c74c3c0157af0621564802186878
Gabriel Costa <g.h.costa@outlook.com>
2022-01-17 16:53:36 -0300

    Adicionando o arquivo forca.py

---
+++ projeto-melhorado/forca.py
@@ -0,0 +1,16 @@
+
+
+
+#Função de execução do programa
+def executar():
+
+    #Título do jogo
+    print("***************************")
+    print("*      Jogo da Forca      *")
+    print("***************************")
+
+    
+
+#Teste para execução do programa
+if(__name__ == "__main__"):
+    executar()
